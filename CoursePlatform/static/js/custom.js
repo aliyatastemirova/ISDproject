b3b5a421e8c1eb2c7afdfcccccf5dff157d476a4
aliyatas <39190883+aliyatas@users.noreply.github.com>
2020-05-29 18:52:13 +0200

    Active class for tabs fix

--- CoursePlatform/static/js/custom.js
+++ CoursePlatform/static/js/custom.js
@@ -64,6 +64,25 @@ $("document").ready(function ($) {
 
     });
 
+    var btnContainer = document.getElementById("nav-tab");
+
+    // Get all buttons with class="btn" inside the container
+    var btns = btnContainer.getElementsByClassName("nav-item");
+
+    // Loop through the buttons and add the active class to the current/clicked button
+    for (var i = 0; i < btns.length; i++) {
+      btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+
+        // If there's no active class
+        if (current.length > 0) {
+          current[0].className = current[0].className.replace(" active", "");
+        }
+
+        // Add the active class to the current/clicked button
+        this.className += " active";
+      });
+    }
 
     // counter js
     $('.counter').counterUp({
@@ -125,7 +144,5 @@ $("document").ready(function ($) {
 
         });
     });
-
-  
 
 });
